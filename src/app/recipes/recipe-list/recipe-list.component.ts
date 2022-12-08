23a2717b36b65a23ae05d505833e5a989283e14c
Xiangyu Liu <neulxy@gmail.com>
2022-12-09 09:51:45 +1300

    fetch recipes using recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,23 +8,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Recipe A',
-      'This is a simple test A',
-      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg'
-    ),
-    new Recipe(
-      'Recipe B',
-      'This is a simple test B',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/group_final-a6ddb3e.jpg'
-    ),
-    new Recipe(
-      'Recipe C',
-      'This is a simple test C',
-      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg'
-    ),
-  ];
+  recipes: Recipe[] = [];
 
   @Output() recipeClick = new EventEmitter<Recipe>();
 
@@ -31,7 +16,9 @@ export class RecipeListComponent implements OnInit {
     this.recipeClick.emit(recipe);
   }
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,25 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Recipe A',
+      'This is a simple test A',
+      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg'
+    ),
+    new Recipe(
+      'Recipe B',
+      'This is a simple test B',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/group_final-a6ddb3e.jpg'
+    ),
+    new Recipe(
+      'Recipe C',
+      'This is a simple test C',
+      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); //by using slice, just return a copy of the recipe array and do not touch the original array
+  }
+}
