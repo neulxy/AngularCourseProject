2d672e594246583068b370c8b873b6b4c844f679
Xiangyu Liu <neulxy@gmail.com>
2023-01-04 11:57:30 +1300

    transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,26 +5,28 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Fried Chicken',
-      'This is a simple test A',
-      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg',
-      [new Ingredient('Chicken', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Salad',
-      'This is a simple test B',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/group_final-a6ddb3e.jpg',
-      [new Ingredient('Apple', 2), new Ingredient('Tomatoes', 2)]
-    ),
-    new Recipe(
-      'Recipe C',
-      'This is a simple test C',
-      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg',
-      []
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Fried Chicken',
+  //     'This is a simple test A',
+  //     'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg',
+  //     [new Ingredient('Chicken', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Salad',
+  //     'This is a simple test B',
+  //     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/group_final-a6ddb3e.jpg',
+  //     [new Ingredient('Apple', 2), new Ingredient('Tomatoes', 2)]
+  //   ),
+  //   new Recipe(
+  //     'Recipe C',
+  //     'This is a simple test C',
+  //     'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/1:1/w_2240,c_limit/RoastChicken_RECIPE_080420_37993.jpg',
+  //     []
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -18,8 +19,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(`${this.url}/recipes.json`).subscribe((recipes) => {
-      this.recipeService.setRecipes(recipes);
-    });
+    this.http
+      .get<Recipe[]>(`${this.url}/recipes.json`)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => ({
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          }));
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
