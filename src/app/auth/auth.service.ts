fb98aced40f763404d60ec4a480689bb883e8b2d
Xiangyu Liu <neulxy@gmail.com>
2023-01-05 11:59:17 +1300

    sending login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -36,5 +37,28 @@ export class AuthService {
           return throwError(errorMessage);
         })
       );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCibesGL8d2HILWv4aYSDWAu8M8MER5LkI',
+      { email: email, password: password, returnSecureToken: true }
+    );
+    // .pipe(
+    //   catchError((errorRes) => {
+    //     let errorMessage = 'An unknown error occured!';
+
+    //     if (!errorRes.error || !errorRes.error.error) {
+    //       return throwError(errorMessage);
+    //     }
+
+    //     switch (errorRes.error.error.message) {
+    //       case 'EMAIL_EXISTS':
+    //         errorMessage = 'This email exists already';
+    //     }
+
+    //     return throwError(errorMessage);
+    //   })
+    // );
   }
 }
