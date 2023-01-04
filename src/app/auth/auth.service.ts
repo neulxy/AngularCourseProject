7023a046921e51ac3e711f7815c40d5622d81197
Xiangyu Liu <neulxy@gmail.com>
2023-01-05 11:34:58 +1300

    improve error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
   idToken: string;
@@ -13,9 +15,26 @@ interface AuthResponseData {
 export class AuthService {
   constructor(private http: HttpClient) {}
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCibesGL8d2HILWv4aYSDWAu8M8MER5LkI',
-      { email: email, password: password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCibesGL8d2HILWv4aYSDWAu8M8MER5LkI',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occured!';
+
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+          }
+
+          return throwError(errorMessage);
+        })
+      );
   }
 }
