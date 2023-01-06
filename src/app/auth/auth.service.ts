0ea58b2072b9d87bdfbb30450c57465cd0287bf7
Xiangyu Liu <neulxy@gmail.com>
2023-01-06 15:28:48 +1300

    adding auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -57,6 +57,29 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
