8952e76507ec5bb36c3c438474000959e101cced
Xiangyu Liu <xiangyu.liu@mitre10.co.nz>
2023-01-10 10:38:59 +1300

    creating Shared Modules

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
@@ -22,8 +22,8 @@ import { RecipesComponent } from './recipes.component';
   imports: [
     ReactiveFormsModule,
     RouterModule,
-    CommonModule,
     RecipesRoutingModule,
+    SharedModule,
   ],
 })
 export class RecipesModule {}
