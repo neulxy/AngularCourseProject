09a1f40ea3fd99678dc6b73a095167279dd815cc
Xiangyu Liu <xiangyu.liu@mitre10.co.nz>
2023-01-10 14:35:10 +1300

    lazy loading AuthModule and ShoppingListModule

--- src/app/shopping-list/shpping-list.module.ts
+++ src/app/shopping-list/shpping-list.module.ts
@@ -8,9 +8,7 @@ import { ShoppingListComponent } from './shopping-list.component';
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingEditComponent],
   imports: [
-    RouterModule.forChild([
-      { path: 'shopping-list', component: ShoppingListComponent },
-    ]),
+    RouterModule.forChild([{ path: '', component: ShoppingListComponent }]),
     FormsModule,
     SharedModule,
   ],
